823eca5696c1815996126e4de9c302eb45d62ade
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-06-17 16:08:59 +0800

    Server can take two clients but no communication yet

--- lab-act4/ChatClient.java
+++ lab-act4/ChatClient.java
@@ -7,20 +7,21 @@ public class ChatClient
 	{
 		String sServerAddress = args[0];
 		int nPort = Integer.parseInt(args[1]);
-		String name = args[2];
-	
+		String username = args[2];
+		String message = args[3];
 		try
 		{
 			Socket clientEndpoint = new Socket(sServerAddress, nPort);
-			System.out.println(name + ": Connecting to server at" + clientEndpoint.getRemoteSocketAddress());
-			System.out.println(name + ": Connected to server at" + clientEndpoint.getRemoteSocketAddress());
 			
+			System.out.println(username + ": Connecting to server at" + clientEndpoint.getRemoteSocketAddress());
+			System.out.println(username + ": Connected to server at" + clientEndpoint.getRemoteSocketAddress());
 			DataOutputStream dosWriter = new DataOutputStream(clientEndpoint.getOutputStream());
-			dosWriter.writeUTF("Client: Hello from client" + clientEndpoint.getLocalSocketAddress());
 			
+			dosWriter.writeUTF("Client: Hello from client" + clientEndpoint.getLocalSocketAddress());
+			dosWriter.writeUTF(message);
 			DataInputStream disReader = new DataInputStream(clientEndpoint.getInputStream());
 			System.out.println(disReader.readUTF());
-			
+		
 			clientEndpoint.close();
 		}
 		catch (Exception e)
--- lab-act4/ChatServer.java
+++ lab-act4/ChatServer.java
@@ -6,24 +6,29 @@ public class ChatServer
 	public static void main(String[] args)
 	{
 		int nPort = Integer.parseInt(args[0]);
-		System.out.println("Server: Listening on port " + args[0] + "...");
+		
 		ServerSocket serverSocket;
 		Socket serverEndpoint;
 
 		try 
 		{
-			serverSocket = new ServerSocket(nPort);
-			serverEndpoint = serverSocket.accept();
+		
 			
-			System.out.println("Server: New client connected: " + serverEndpoint.getRemoteSocketAddress());
+			while(true){
+				serverSocket = new ServerSocket(nPort);
+				System.out.println("Server: Listening on port " + args[0] + "...");
+				serverEndpoint = serverSocket.accept();
+				System.out.println("Server: New client connected: " + serverEndpoint.getRemoteSocketAddress());
+				DataInputStream disReader = new DataInputStream(serverEndpoint.getInputStream());
+				System.out.println(disReader.readUTF());
+				System.out.println(disReader.readUTF());
+			}
 			
-			DataInputStream disReader = new DataInputStream(serverEndpoint.getInputStream());
-			System.out.println(disReader.readUTF());
-			
-			DataOutputStream dosWriter = new DataOutputStream(serverEndpoint.getOutputStream());
-			dosWriter.writeUTF("Server: Hello World!");
 
-			serverEndpoint.close();
+
+		//	DataOutputStream dosWriter = new DataOutputStream(serverEndpoint.getOutputStream());
+		//	dosWriter.writeUTF("Server: Hello World!");
+
 		}
 		catch (Exception e)
 		{
