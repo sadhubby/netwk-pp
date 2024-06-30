90f19ff3a9b72585f29e29d87061d1f3ffd3d814
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-06-30 20:18:24 +0800

    Looks like its working

--- lab-act5/CSNETWK-WebServer.py
+++ lab-act5/CSNETWK-WebServer.py
@@ -2,24 +2,30 @@
 from socket import *
 import sys # In order to terminate the program
 
+serverPort = 4000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
+serverSocket.bind(("", serverPort))
+serverSocket.listen(1)
 #Fill in end
 
 
 while True:
     #Establish the connection
     print('CSNETWK Web Server is ready to serve...')
-    connectionSocket, addr = #Fill in start     #Fill in end
+    connectionSocket, addr = serverSocket.accept()#Fill in start   #Fill in end
     try:
-        message = #Fill in start #Fill in end
+        message = connectionSocket.recv(1024)#Fill in start #Fill in end
         filename = message.split()[1]
         f = open(filename[1:])
-        outputdata = #Fill in start #Fill in end
+        outputdata = f.read()#Fill in start #Fill in end
         #Send one HTTP header line into socket
         #Fill in start
+        ok = "HTTP/1.1 200 OK\r\n\r\n"
+        connectionSocket.send(ok.encode())
         #Fill in end
+
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
@@ -29,12 +35,15 @@ while True:
     
     except IOError:
         #Send response message for file not found
-        #Fill in start
+        #Fill in 
+        notFound = "HTTP/1.1 404 Not Found\r\n\r\n"
+        connectionSocket.send(notFound.encode())
+        
         #Fill in end
        
         #Close client socket
         #Fill in start
-        
+        connectionSocket.close()
         #Fill in end
-serverSocket.close()
-sys.exit()  #Terminate the program after sending the corresponding data
+        serverSocket.close()
+        sys.exit()  #Terminate the program after sending the corresponding data
