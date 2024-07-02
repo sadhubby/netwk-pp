1eebac82067685214510aea2ad28e06165c18f5f
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-07-02 14:58:51 +0800

    Removed comments

--- lab-act5/CSNETWK-WebServer.py
+++ lab-act5/CSNETWK-WebServer.py
@@ -6,7 +6,7 @@ serverPort = 4000 #server port
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
-serverSocket.bind(("", serverPort)) #wants the pair of the address and port number. just do localhost:4000/filename.file sa search bar
+serverSocket.bind(("", serverPort)) 
 serverSocket.listen(1)
 #Fill in end
 
@@ -14,30 +14,31 @@ serverSocket.listen(1)
 while True:
     #Establish the connection
     print('CSNETWK Web Server is ready to serve...')
-    connectionSocket, addr = serverSocket.accept()#Fill in start   #Fill in end #accepts new connection from client, in this case, the web page
+    connectionSocket, addr = serverSocket.accept()#Fill in start   #Fill in end 
     try: 
-        message = connectionSocket.recv(1024).decode()#Fill in start #Fill in end #gets the path of the requested file, and makes it bytes
-        filename = message.split()[1] #reads the entire filename
-        f = open(filename[1:]) #opens the filename
-        outputdata = f.read()#Fill in start #Fill in end #reads the contents of the file, assigns to outputdata
+        message = connectionSocket.recv(1024).decode()#Fill in start #Fill in end 
+        filename = message.split()[1]
+        f = open(filename[1:]) 
+        outputdata = f.read()#Fill in start #Fill in end 
         f.close()
         #Send one HTTP header line into socket
         #Fill in start
-        ok = "HTTP/1.1 200 OK\r\n\r\n" #assigning ok to have this string
-        connectionSocket.send(ok.encode()) #connectionSocket.send only allows bytes, so we do the encode() function 
+        ok = "HTTP/1.1 200 OK\r\n\r\n"
+        
+        connectionSocket.send(ok.encode()) 
         #Fill in end
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
-            connectionSocket.send(outputdata[i].encode()) #so essentially, we give the client (webpage) the contents of outputdata
+            connectionSocket.send(outputdata[i].encode())
        
-        connectionSocket.send("\r\n".encode()) #I actually dont know what \r\n means but this is always shown sa wireshark
+        connectionSocket.send("\r\n".encode()) 
         connectionSocket.close()
     
     except IOError:
         #Send response message for file not found
         #Fill in start
-        notFound = "HTTP/1.1 404 Not Found\r\n\r\n" # same things as sa ok string
+        notFound = "HTTP/1.1 404 Not Found\r\n\r\n" 
         connectionSocket.send(notFound.encode())
         htmlNotFound = "<html><head></head><body><h1>404 Not Found</h1></body></html>"
         connectionSocket.send(htmlNotFound.encode()) 
